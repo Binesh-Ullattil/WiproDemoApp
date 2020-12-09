aaded6a0624c107e94b79ef6481e21e957cdcad3
Binesh <binesh@timeline.ae>
2020-12-10 01:34:09 +0530

    unit test

---
+++ app/src/test/java/com/binesh/wiprodemo/repository/CountryFeedRepositoryTest.kt
@@ -0,0 +1,86 @@
+package com.binesh.wiprodemo.repository
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import com.binesh.wiprodemo.App
+import com.binesh.wiprodemo.apis.ApiService
+import com.binesh.wiprodemo.helper.NetworkStatusHelper
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.core.classloader.annotations.PowerMockIgnore
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+@PowerMockIgnore("javax.net.ssl.*")
+@PrepareForTest(
+    NetworkStatusHelper::class,
+    CountryFeedRepository::class,
+    App::class,
+    NetworkInfo::class,
+    ConnectivityManager::class
+)
+class CountryFeedRepositoryTest {
+    private var countryFeedRepository: CountryFeedRepository? = null
+    @Mock
+    var mContext: Context? = null
+    @Mock
+    var networkStatusHelper: NetworkStatusHelper? = null
+    @Mock
+    var app: App? = null
+    @Mock
+    var mNetworkInfo: NetworkInfo? = null
+    @Mock
+    var connectivityManager: ConnectivityManager? = null
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        PowerMockito.mockStatic(NetworkStatusHelper::class.java)
+        networkStatusHelper!!.init(app!!)
+        PowerMockito.`when`(app!!.getSystemService(Context.CONNECTIVITY_SERVICE))
+            .thenReturn(connectivityManager)
+        countryFeedRepository = CountryFeedRepository(ApiService(mContext))
+    }
+
+    @Test
+    fun testLoadFeedsIfCase() {
+        PowerMockito.`when`(connectivityManager!!.activeNetworkInfo)
+            .thenReturn(mNetworkInfo)
+        PowerMockito.`when`(networkStatusHelper!!.isNetworkAvailable())
+            .thenReturn(true)
+        val result =
+            countryFeedRepository!!.loadFeeds()
+        Assert.assertNotNull(result)
+    }
+
+    @Test
+    fun testLoadFeedsElseCase() {
+        PowerMockito.`when`(connectivityManager!!.activeNetworkInfo)
+            .thenReturn(mNetworkInfo)
+        PowerMockito.`when`(networkStatusHelper!!.isNetworkAvailable())
+            .thenReturn(false)
+        val result =
+            countryFeedRepository!!.loadFeeds()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testGetApiService() {
+        val result = countryFeedRepository!!.apiService
+        Assert.assertNotNull(result)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testSetApiService() {
+        countryFeedRepository!!.apiService = ApiService(mContext)
+    }
+}
---
+++ app/src/test/java/com/binesh/wiprodemo/viewModel/FeedViewModelTest.kt
@@ -0,0 +1,113 @@
+package com.binesh.wiprodemo.viewModel
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import androidx.lifecycle.MutableLiveData
+import com.binesh.wiprodemo.App
+import com.binesh.wiprodemo.apis.ApiService
+import com.binesh.wiprodemo.helper.NetworkStatusHelper
+import com.binesh.wiprodemo.helper.ToastState
+import com.binesh.wiprodemo.repository.CountryFeedRepository
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.core.classloader.annotations.PowerMockIgnore
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+@PowerMockIgnore("javax.net.ssl.*")
+@PrepareForTest(
+    NetworkStatusHelper::class,
+    CountryFeedRepository::class,
+    App::class,
+    NetworkInfo::class,
+    ConnectivityManager::class
+)
+class FeedViewModelTest {
+    @Mock
+    var countryFeedRepository: CountryFeedRepository? = null
+    @Mock
+    var mContext: Context? = null
+    @Mock
+    var networkStatusHelper: NetworkStatusHelper? = null
+    @Mock
+    var app: App? = null
+    @Mock
+    var mNetworkInfo: NetworkInfo? = null
+    @Mock
+    var connectivityManager: ConnectivityManager? = null
+    @Mock
+    var toastState: MutableLiveData<ToastState>? = null
+    @Mock
+    var mBagOfTags: Map<String, Any>? = null
+    @InjectMocks
+    var feedViewModel: FeedViewModel? = null
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        PowerMockito.mockStatic(NetworkStatusHelper::class.java)
+        networkStatusHelper!!.init(app!!)
+        PowerMockito.`when`(app!!.getSystemService(Context.CONNECTIVITY_SERVICE))
+            .thenReturn(connectivityManager)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testGetFeeds() {
+        val result =
+            feedViewModel!!.getFeeds()
+        Assert.assertEquals(null, result)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testGetRepository() {
+        val result = feedViewModel!!.repository
+        Assert.assertNotNull(result)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testSetRepository() {
+        feedViewModel!!.repository = CountryFeedRepository(ApiService(null))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testGetToastState() {
+        val result = feedViewModel!!.toastState
+        Assert.assertNotNull(result)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testSetToastState() {
+        feedViewModel!!.toastState = toastState!!
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testSetInfoToast() {
+        feedViewModel!!.setInfoToast(0)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testSetSuccessToast() {
+        feedViewModel!!.setSuccessToast(0)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testSetFailureToast() {
+        feedViewModel!!.setFailureToast(0)
+    }
+}
